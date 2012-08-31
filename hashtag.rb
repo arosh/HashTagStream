fea07c2005e572a4df0f63fecbc87252eca89041
Sho Iizuka <sho.i518@gmail.com>
2012-08-31 23:36:16 +0900

    エラーをエラー出力に表示するように変更
    未テストにつき注意。
    他には、メソッドを自分好みに変更したり。

--- hashtag.rb
+++ hashtag.rb
@@ -1,5 +1,7 @@
 #最初に#なしのハッシュタグ入れるとscreen_nameとtweetが出てきます
 #適当に書き換えればいいと思うよ。
+
+# gem install twitter-stream -s http://gemcutter.org
 # -*- encoding: UTF-8 -*-
 
 require 'twitter/json_stream'
@@ -15,12 +17,12 @@ OAUTH_SECRET    = ""
 Twitter.configure do |config|
 	config.consumer_key       = CONSUMER_KEY
 	config.consumer_secret    = CONSUMER_SECRET
-	config.oauth_token			  = OAUTH_TOKEN
+	config.oauth_token        = OAUTH_TOKEN
 	config.oauth_token_secret = OAUTH_SECRET
 end
 
-puts "Please input tags without \'#\'"
-TAG = gets.chomp
+print 'Please input tags without #: '
+TAG = gets.strip
 
 EventMachine::run do
 	userstream = {
@@ -29,10 +31,10 @@ EventMachine::run do
 		:port => 443,
 		:ssl => true,
 		:oauth => {
-			:consumer_key		 => CONSUMER_KEY,
-			:consumer_secret => CONSUMER_SECRET,
-			:access_key			 => OAUTH_TOKEN,
-			:access_secret	 => OAUTH_SECRET
+			:consumer_key     => CONSUMER_KEY,
+			:consumer_secret  => CONSUMER_SECRET,
+			:access_key       => OAUTH_TOKEN,
+			:access_secret    => OAUTH_SECRET
 		}
 	}
 
@@ -43,25 +45,25 @@ EventMachine::run do
 		next if event["direct_message"]
 		next if event["delete"]
 		next if event["friends"]
-		if event['entities']['hashtags'] != [] && 
+		if event['entities']['hashtags'].empty? == false &&
 			event['entities']['hashtags'][0]['text'] == TAG then
-				puts event['user']['screen_name']+" "+event['text']
+				puts event['user']['screen_name'] + " " + event['text']
 		end
 	end
 
 	stream.on_error do |message|
-		$stdout.print "error: #{message}\n"
-		$stdout.flush
+		$stderr.puts "error: #{message}"
+		$stderr.flush
 	end
 
 	stream.on_reconnect do |timeout, retries|
-		$stdout.print "reconnecting in: #{timeout} seconds\n"
-		$stdout.flush
+		$stderr.puts "reconnecting in: #{timeout} seconds"
+		$stderr.flush
 	end
 
 	stream.on_max_reconnects do |timeout, retries|
-		$stdout.print "Failed after #{retries} failed reconnects\n"
-		$stdout.flush
+		$stderr.puts "Failed after #{retries} failed reconnects"
+		$stderr.flush
 	end
 
 	trap('TERM') {
